f426672502cf2bf252dc26aa3d8843b328d5581e
Saddock Kabandana <aimegetz@gmail.com>
2026-07-16 13:42:16 +0200

    Fix email copyright year and UZA Bulk branding

--- helper/emailTemplateHtml/getTemplateFooter.js
+++ helper/emailTemplateHtml/getTemplateFooter.js
@@ -3,7 +3,7 @@ module.exports = `
 <tbody>
   <tr>
     <td align="left">
-    <p style="font-family: 'open sans', 'helvetica neue', helvetica, arial, sans-serif;color: #505068;padding: 0 50px;font-size:13px;margin-top:15px;line-height:24px;margin-bottom:0px;">Copyright &copy; 2021 All rights reserved to [storeName]</p>
+    <p style="font-family: 'open sans', 'helvetica neue', helvetica, arial, sans-serif;color: #505068;padding: 0 50px;font-size:13px;margin-top:15px;line-height:24px;margin-bottom:0px;">Copyright &copy; [YEAR] All rights reserved to UZA Bulk</p>
     </td>
   </tr>
 </tbody>
